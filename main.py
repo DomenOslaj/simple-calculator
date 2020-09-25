00cd5998db68dd13c33e7446727502895587ef32
Domen Oslaj <doslaj@gmail.com>
2020-09-25 23:34:05 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 num1 = int(input("Please enter a first number: "))
 num2 = int(input("Please enter a second number: "))
+operation = input("Please choose one from the following operations (+, -, *, /) ")
+
+if operation == "+":
+    result = str(num1 + num2)
+    print("Result: " + result)
+
+elif operation == "-":
+    result = str(num1 - num2)
+    print("Result: " + result)
+
+elif operation == "*":
+    result = str(num1 * num2)
+    print("Result: " + result)
+
